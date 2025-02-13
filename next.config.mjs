0bee80521150e5edc8b59f9829471f040e5c99a9
Anıl ÇALIŞKAN <naksilaclina@gmail.com>
2025-02-13 17:28:24 +0300

    Update NextJS config

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,16 @@ const nextConfig = {
     reactStrictMode: true,
 }
 
-export default nextConfig 
+// LD_LIBRARY_PATH ayarını ekleyin
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
+export default nextConfig;
